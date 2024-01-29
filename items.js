217a6cea148149a69e20a6ff46ce7b314ed623c2
Anastasia <nastazka8@gmail.com>
2024-01-29 17:51:51 +0300

    модификация items

--- items.js
+++ items.js
@@ -1,4 +1,6 @@
 let data;
+const cart = document.querySelector(".cart");
+cart.style.display = "none";
 
 async function productData () {
   try
@@ -21,57 +23,58 @@ async function productData () {
             <p class="description">${data[id].desc}</p>
           </div>
           <p class="price">${data[id].price}</p>
-          <p>${id}</p>
+          <p class="product_id">${id}</p>
         </div>       
       `
       container.insertAdjacentHTML("beforeend", productCard);
 
       const button = document.createElement("button");
+      button.classList.add("addToCart-btn");
       button.textContent = "Добавить в корзину";
       button.addEventListener("click", function () {
         addProductToCart(id);
       })
-      container.lastElementChild.appendChild(button);
-      
-      
-    };
-        
+      container.lastElementChild.appendChild(button); 
+    }    
   }
   catch (error) {
     console.error(error);
   }
-
 }
-
 
 productData();
 
 function addProductToCart(productId) {
   
-  // Получить корзину.
-  const cart = document.querySelector(".cart");
-  
+  console.log(cart.children.length);
   const card =
   `
     <div class="product_card">
         <img src="${data[productId].img}" class="product-image" alt="product1">
         <a href="./product.html"><h3>${data[productId].name}</h3></a>
         <p class="price">${data[productId].price}</p>
-        <p>${productId}</p>
+        <p class="product_id">${productId}</p>
     </div>
-  `;
-  // Добавить карточку товара в корзину.
+  `
   cart.insertAdjacentHTML("beforeend", card);
 
   const button = document.createElement("button");
-      button.textContent = "Удалить";
-      button.addEventListener("click", function () {
-        // removeProductFromCart(productId);
-        button.parentElement.remove();
-      })
-      cart.lastElementChild.appendChild(button);
+  button.classList.add("removeFromCart-btn")
+  button.textContent = "Удалить";
+  button.addEventListener("click", function () {
+    button.parentElement.remove();
+    console.log(cart.children.length);
+    if (cart.children.length < 2) {
+      cart.style.display = "none";
+    }
+  })
+  cart.lastElementChild.appendChild(button);
+  
+  if (cart.children.length >= 1) {
+    cart.style.display = "grid";
+  }
 }
 
-if (cart.children.length === 0) {
-  cart.style.display = "none";
-}
+
+
+
